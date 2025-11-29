2eb87935275c2f1fcd29f87510ce510b5609995e
Jayane <176343420+jayane-c@users.noreply.github.com>
2025-11-28 23:54:57 -0300

    feat(server): adicionado o initializeDatabase.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
-
+import { initializeDatabase } from './config/database';
 
 const app = express();
-
 app.listen(3000, () => console.log('Servidor rodando na porta 3000.'))
+
+initializeDatabase().catch((error) => console.error('Erro ao conectar ao banco de dados:', error));
+
