ffa4868e828cb5eba61e5b0ca3cc08f525c6b26b
Narayana Alves <narayanaalves098@gmail.com>
2025-12-22 19:27:49 -0300

    feat: cria estrutura inicial das páginas de cadastro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 //import { useState } from 'react'
 import Home from './Components/Home/home'
-
+import CadastroMorador from './Pages/Cadastro/Morador/CadastroMorador'
 
 function App() {
  
   return (
     <div>
       <Home/>
+      <CadastroMorador/>
       
     
 
