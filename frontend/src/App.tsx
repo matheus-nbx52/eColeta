96e49ca4153db89fa6f7e487da802391d068f5c2
Narayana Alves <narayanaalves098@gmail.com>
2025-12-27 13:41:32 -0300

    feat: adiciona rotas de cadastro (morador, coletor e cooperativa)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 //import { useState } from 'react'
-import Home from './Components/Home/home'
-import CadastroMorador from './pages/cadastro/morador/CadastroMorador'
-import CadastroColetor from './pages/cadastro/coletor/CadastroColetor'
-import CadastroCooperativa from './pages/cadastro/cooperativa/CadastroCooperativa'
+import Home from './pages/home/home'
+import AppRoutes from './routes/AppRoutes'
 
 
 function App() {
@@ -10,14 +8,7 @@ function App() {
   return (
     <div>
       <Home/>
-      <CadastroMorador/>
-      <CadastroColetor/>
-      <CadastroCooperativa/>
-      
-    
-
-     
-
+      <AppRoutes/>
     </div>
   )
 }
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,1 +1,20 @@
-export {}
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom"
+
+import CadastroMorador from "../pages/cadastro/morador/CadastroMorador"
+import CadastroColetor from "../pages/cadastro/coletor/CadastroColetor"
+import CadastroCooperativa from "../pages/cadastro/cooperativa/CadastroCooperativa"
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/cadastro-morador" element={<CadastroMorador />} />
+                <Route path="/cadastro-coletor" element={<CadastroColetor />} />
+                <Route path="/cadastro-cooperativa" element={<CadastroCooperativa />} />
+            </Routes>
+        </BrowserRouter>
+
+    )
+}
+
+export default AppRoutes;
