cd3f8ab8663da56f6ee04e7143f170eb93e93f6e
Debora Milena <deboramilenasn@gmail.com>
2025-12-30 18:30:24 -0300

    style: ajusta layout das páginas home e dashboard morador

---
+++ frontend/src/pages/dashboard/morador/DashboardMorador.tsx
@@ -0,0 +1,17 @@
+import './DashboardMorador.css';
+import NavbarMorador from '../../../Components/navbar-morador/NavbarMorador';
+import DashboardContentMorador from '../../../Components/dashboardContent/DashboardContentMorador';
+
+export default function DashboardMorador() {
+ 
+    const nomeDoUsuarioLogado = "Débora"; 
+
+    return (
+        <div className="recipiente-dashboard-principal">
+           
+            <NavbarMorador nome={nomeDoUsuarioLogado} />
+            
+            <DashboardContentMorador />
+        </div>
+    );
+}
