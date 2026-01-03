b5bcc1da3d3738be6e5af7957370a56f2a42147b
Narayana Alves <narayanaalves098@gmail.com>
2026-01-03 13:26:56 -0300

    feat: adiciona e centraliza dados de coletas disponiveis, e adiciona cards com opção de aceitar ou recusar

--- frontend/src/Components/dashboard-coletor/coletas-disponiveis/ColetasDisponiveis.tsx
+++ frontend/src/Components/dashboard-coletor/coletas-disponiveis/ColetasDisponiveis.tsx
@@ -1,38 +1,47 @@
 import "./ColetasDisponiveis.css";
+import { Check, X } from 'lucide-react';
 
 function ColetasDisponiveis() {
   return (
-    
-
     <div className="coletas-container">
-      <h2>Coletas disponíveis</h2>
-
+      <h2 className="titulo-secao">Coletas disponíveis</h2>
       <div className="coleta-card">
-        <div>
-          <strong>Material:</strong> Papel e Plástico
+       
+        <div className="coleta-info">
+          <div><strong>Material:</strong> Papel e Plástico</div>
+          <div><strong>Peso est. (Kg):</strong> 5kg</div>
+          <div><strong>Endereço:</strong> Rua das Flores, 123</div>
+          <div><strong>Data:</strong> 20/09/2025</div>
+          <div><strong>Horário:</strong> 14:00</div>
         </div>
-        <div>
-          <strong>Endereço:</strong> Rua das Flores, 123
+        
+        <div className="coleta-actions">
+          <button className="btn-aceitar">
+            <Check size={18} /> Aceitar
+          </button>
+          <button className="btn-recusar">
+            <X size={18} /> Recusar
+          </button>
         </div>
-        <div>
-          <strong>Data:</strong> 20/09/2025
-        </div>
-
-        <button className="btn-coleta">Ver detalhes</button>
       </div>
 
       <div className="coleta-card">
-        <div>
-          <strong>Material:</strong> Vidro
-        </div>
-        <div>
-          <strong>Endereço:</strong> Av. Central, 456
-        </div>
-        <div>
-          <strong>Data:</strong> 22/09/2025
+        <div className="coleta-info">
+          <div><strong>Material:</strong> Vidro</div>
+          <div><strong>Peso est. (Kg):</strong> 5kg</div>
+          <div><strong>Endereço:</strong> Av. Central, 456</div>
+          <div><strong>Data:</strong> 22/09/2025</div>
+          <div><strong>Horário:</strong> 14:00</div>
         </div>
 
-        <button className="btn-coleta">Ver detalhes</button>
+        <div className="coleta-actions">
+          <button className="btn-aceitar">
+            <Check size={18} /> Aceitar
+          </button>
+          <button className="btn-recusar">
+            <X size={18} /> Recusar
+          </button>
+        </div>
       </div>
     </div>
   );
